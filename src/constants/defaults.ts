f9d5db22255b8da7842c4e03287d74d26d89b300
jakegsy <jake@jakegsy.com>
2024-12-17 03:42:26 +0700

    update constants to june's request

--- src/constants/defaults.ts
+++ src/constants/defaults.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_NETWORK = 'ethereum';
 export const DEFAULT_LIQUIDITY_TYPE = 'uniswap_v3';
-export const DEFAULT_PRICE_RANGE = '10%'; 
-export const DEFAULT_BASE_TOKEN = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-export const DEFAULT_POOL_ADDRESS = '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8';
+export const DEFAULT_PRICE_RANGE = '1%'; 
+export const DEFAULT_BASE_TOKEN = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+export const DEFAULT_POOL_ADDRESS = '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640';
