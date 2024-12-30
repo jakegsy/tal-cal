9f0437cecc8df0ac0024aa3be319f910a2fb0618
jakegsy <jake@jakegsy.com>
2024-12-30 10:57:25 +0000

    added 2% milk

--- src/components/SimpleSelect/PriceRangeSelector.tsx
+++ src/components/SimpleSelect/PriceRangeSelector.tsx
@@ -3,7 +3,7 @@ interface PriceRangeSelectorProps {
   onChange: (value: string) => void;
 }
 
-const ranges = ['0.1%', '0.5%', '1%', '5%', '10%'];
+const ranges = ['0.1%', '0.5%', '1%', '2%', '5%', '10%'];
 
 export function PriceRangeSelector({ value, onChange }: PriceRangeSelectorProps) {
   return (
